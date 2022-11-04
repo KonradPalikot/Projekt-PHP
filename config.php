541be6f78c7d0587fd59935ef6897fd06c156c89
SzymonSolomianko <116341751+SzymonSolomianko@users.noreply.github.com>
2022-11-04 08:55:11 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php
 $sql = "SELECT * FROM projekt_PHP WHERE email ='$email' AND password='$haslo'"
 $result = $connection -> query($sql)
+$connection = new mysqli($host, $db_user, $db_password, $db_name);
 ?>
