c7913735adc4c9defdb1d06263557a3c057a08a1
SzymonSolomianko <116341751+SzymonSolomianko@users.noreply.github.com>
2022-12-02 09:03:24 +0100

    .
    rejstracja

--- mainClass.php
+++ mainClass.php
@@ -12,5 +12,3 @@ class MainClass {
     }
 }
 ?>
-
-
---
+++ rejstracja.php
@@ -0,0 +1,8 @@
+<form method="Post" action="rejstracja.php"
+    <b>Login:</b> <input type="text" name="login"> <br><br>
+    <b>Haslo:</b> <input type="password" name"Haslo"><br>
+    <b>powtorz haslo:</b> <input type="password" name="powtorz haslo"><br><br>
+    <b>Email:</b> <input type="text" name="email"><br>
+    <input type="submit" value="Utworz konto" name="rejestruj">
+</form>
+ 
