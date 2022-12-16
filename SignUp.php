9a8abeb07aa6ec3f386b905a4c011483ac09064c
KonradPalikot <105857127+KonradPalikot@users.noreply.github.com>
2022-12-16 09:06:35 +0100

    Rejestracja

--- SignUp.php
+++ SignUp.php
@@ -6,38 +6,16 @@
     $connection = $mainClass -> dbConnect();
 
     if($connection -> connect_errno == 0) {
-<<<<<<< HEAD
-        $userEmail = htmlentities($_POST['userEmail'], ENT_QUOTES, "UTF-8");
-        $userPassword = htmlentities($_POST['userPassword'], ENT_QUOTES, "UTF-8");
-=======
-        $userName = htmlentities($_POST['name'], ENT_QUOTES, "UTF-8");
+        $userName = htmlentities($_POST['userName'], ENT_QUOTES, "UTF-8");
         $userEmail = htmlentities($_POST['userEmail'], ENT_QUOTES, "UTF-8");
         $userPassword = htmlentities($_POST['userPassword'], ENT_QUOTES, "UTF-8");
         $userPassword2 = htmlentities($_POST['userPassword2'], ENT_QUOTES, "UTF-8");
->>>>>>> efe7a08cb38bdf438b4e35e8554cc6de449749f2
 
         $sql =sprintf(
-        "SELECT * FROM php_projekt WHERE email='$userEmail' AND haslo='$userPassword'",
-        mysqli_real_escape_string($connection, $userEmail),
-<<<<<<< HEAD
-        mysqli_real_escape_string($connection, $userPassword));
-
-        if($result = $connection -> query($sql)) {
-            if($result -> num_rows > 0) {
-                $data = $result -> fetch_assoc();
-                $user = $data['login'];
-                $email = $data['email'];
-                $_SESSION['signedIn'] = true;
-                unset($_SESSION['signInError']);
-                header('Location: index.php');
-                
-                $result -> close();
-            } else {
-                header('Location: signInPage.php');
-                $_SESSION['signInError'] = true;
-=======
-        mysqli_real_escape_string($connection, $userPassword)
-    );
+            "SELECT * FROM php_projekt WHERE email='$userEmail' AND haslo='$userPassword'",
+            mysqli_real_escape_string($connection, $userEmail),
+            mysqli_real_escape_string($connection, $userPassword)
+        );
 
         if($result = $connection -> query($sql)) {
             if($result -> num_rows > 0) {
@@ -45,19 +23,18 @@
             }
             else {
                 if($userPassword == $userPassword2) {
-                    if(strlen($userPassword) >= 8)
-                        $sql = "INSERT INTO php_projekt(ID, login, haslo, email) VALUES ('', '$userName', '$userPassword', '$userEmail')";
+                    if(strlen($userPassword) >= 8) {
+                        $sql = "INSERT INTO php_projekt(ID, login, haslo, email) VALUES (null, '$userName', '$userPassword', '$userEmail')";
                         $result = $connection -> query($sql);
                         echo "Rejestracja powiodła się";
-                        header('Location: index.php');
-                    else {
+                        // header('Location: index.php');
+                    } else {
                         echo "Hasło nie posiada odpowiedniej ilości zanków [8]";
                     }
                 }
                 else{
                     echo "Podane hasła nie są identyczne";
                 }
->>>>>>> efe7a08cb38bdf438b4e35e8554cc6de449749f2
             }
         }
     }
--- signUpPage.php
+++ signUpPage.php
@@ -8,38 +8,17 @@
     <title>Rejestracja</title>
 </head>
 <body>
-<form method="POST" action="signUpPage.php">
+<form method="POST" action="SignUp.php">
     <label>Nazwa użytkownika:</label>
-    <input type="text" name="name">
+    <input type="text" name="userName">
     <label>Email:</label>
-    <input type="email" name="email">
+    <input type="email" name="userEmail">
     <label>Hasło:</label>
-    <input type="text" name="haslo1">
+    <input type="text" name="userPassword">
     <label>Powtórz hasło:</label>
-    <input type="text" name="haslo2">
+    <input type="text" name="userPassword2">
     <input type="submit" value="Ok">
-    <?php
-    session_start();
-    mysql_connect("localhosst", "admin", "haslo");
-    mysql_select_db("baza");
-    
-    function filtr($zmienna) {
-        if(get_magic_quotes_gpc())
-            $zmienna = stripslashes($zmienna);
-        return mysql_real_escape_string(htmlspecialchars(trim($zmienna)));
-        }
-        if (isset($_POST['rejestruj'])) {
-            $login = filtr($_POST['name']);
-            $email = filtr($_POST['email']);
-            $haslo1 = filtr($_POST['haslo1']);
-            $haslo2 = filtr($_POST['haslo2']);
-            if (mysql_num_rows(mysql_query("Select login FROM php_projekt WHERE login = '".$login"';")) == 0) {
-                if ($haslo1 == $haslo2) {
-                    // mysql_query("INSERT INTO 'uzytkownicy' (login, email, haslo)")
-                }
-            }
-        }
-    ?>
+    <a href="index.php">Powrót</a>
 </form>
 </body>
 </html>
